5ad32c18ca39c0671d29a6b2a1d234a478983789
jjgomezr1 <jjgomezr1@eafit.edu.co>
2024-05-10 17:16:09 -0500

    Se agrego un metodo que imprime el nombre del estudiante

---
+++ assignment3/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void message() {
+        System.out.println("Assignment 3 by Juan José Gómez");
+    }
+    public static void main(String[] args) {
+        message();
+    }
+}
